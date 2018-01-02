5bdf9364add5109549f492c55d06db2bd2f6c36e
Chris <chris@cypheros.co>
2018-01-01 22:48:04 -0500

    Settings: Support black theme
    Change-Id: Idf88c571ef7acf6694a96dab3f3f403fac14e565
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/android/settings/display/ThemePreferenceController.java
+++ src/com/android/settings/display/ThemePreferenceController.java
@@ -156,7 +156,8 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
             for (int i = 0, size = infos.size(); i < size; i++) {
                 if (infos.get(i).isEnabled() &&
                         isChangeableOverlay(infos.get(i).packageName) &&
-                        !infos.get(i).packageName.equals("com.android.system.theme.dark")) {
+                        !infos.get(i).packageName.equals("com.android.system.theme.dark") && 
+                        !infos.get(i).packageName.equals("com.android.system.theme.black")) {
                     return infos.get(i).packageName;
                 }
             }
@@ -200,7 +201,8 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
             List<String> pkgs = new ArrayList(infos.size());
             for (int i = 0, size = infos.size(); i < size; i++) {
                 if (isChangeableOverlay(infos.get(i).packageName)) {
-                    if (!infos.get(i).packageName.equals("com.android.system.theme.dark"))
+                    if (!infos.get(i).packageName.equals("com.android.system.theme.dark") && 
+                        !infos.get(i).packageName.equals("com.android.system.theme.black"))
                         pkgs.add(infos.get(i).packageName);
                 }
             }
